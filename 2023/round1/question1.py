c85fed17ec6a04a89bc1aa4ac8adcc19cea56ea5
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2025-07-01 11:11:30 +0100

    update

--- 2023/round1/question1.py
+++ 2023/round1/question1.py
@@ -32,9 +32,33 @@ def calc_num(n, my_list):
 #end procedure
 
 my_list = []
-fibgen(53316291173,my_list)
+#fibgen(53316291173,my_list)
+fibgen(5000000001,my_list)
 print(my_list)
 print(len(my_list))
+
+def threes(my_list):
+    total = []
+    i=0
+    while i < len(my_list) - 4:
+        j = i + 2
+        while j < len(my_list) - 2:
+            k = j + 2
+            while k < len(my_list):
+                total.append(my_list[i]+my_list[j]+my_list[k])
+                #total+=1
+                k+=1
+            j+=1
+        i+=1
+    #end while
+    return total
+#end procedure
+
+ans = threes(my_list)
+ans.sort()
+print(ans[:50])
+
+    
 #print(find_fib(514228,my_list))
 #print(calc_num(514228,my_list))
 # total = 0
